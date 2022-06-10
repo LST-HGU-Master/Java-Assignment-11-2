c0bd0df7b0a1ada400dc4428720b455f333f8ef5
YoshiHirat <50605381+YoshiHirat@users.noreply.github.com>
2022-06-10 16:56:10 +0900

    Update SwordTest.java

--- src/test/java/SwordTest.java
+++ src/test/java/SwordTest.java
@@ -6,7 +6,9 @@ import java.lang.reflect.Modifier;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
-
+/**
+ * @version (20220610)
+ */
 public class SwordTest {
     @Test
     public void testPrivate() {
@@ -15,11 +17,10 @@ public class SwordTest {
             Sword s = new Sword("こんぼう");
             Field name = s.getClass().getDeclaredField("name");
 
-            assertEquals(Modifier.PRIVATE, name.getModifiers());
+            assertEquals(Modifier.PRIVATE, name.getModifiers(),"Swordクラスのフィールドnameがprivate宣言されていません!");
 
-        } catch (Exception e) {
-            // なにかしら例外がでたらfailにする
-            fail();
+        } catch (NoSuchFieldException e) {
+            fail("Swordクラスのフィールドとして name が定義されていません!");
         }
     }
 
@@ -34,10 +35,16 @@ public class SwordTest {
             Field name = s.getClass().getDeclaredField("name");
             name.setAccessible(true);
 
-            assertEquals("銅の剣", name.get(s));
+            assertEquals("銅の剣", name.get(s),"setName()の処理が不正です!");
 
-        } catch (Exception e) {
-
+        } catch (NoSuchMethodException nsme) {
+            fail("SwordクラスにsetName()が定義されていない、もしくはpublic宣言されていません! ");
+        } catch (IllegalAccessException iae) {
+            fail("SwordクラスのsetName()がpublic宣言されていません! ");
+        } catch (java.lang.reflect.InvocationTargetException ite) {
+            fail("setName()内で例外が発生しました"); // 教員対応
+        } catch (NoSuchFieldException nsfe) {
+            fail("nameが宣言されていません!"); // これは発生しえない
         }
     }
 
@@ -52,9 +59,25 @@ public class SwordTest {
             Field name = s.getClass().getDeclaredField("name");
             name.setAccessible(true);
 
-            assertEquals("名無し剣", name.get(s));
+            assertEquals("名無し剣", name.get(s), "２文字以下でのsetName()の処理が不正です!");
         } catch (Exception e) {
-            // なにかしら例外がでたらfailにする
+            // NoSuchMethodException, IllegalAccessException iae はtestSetter()でチェックしているので、ここでは無視
+            fail();
+        }
+    }
+
+    @Test
+    public void testSetter3() {
+        try{
+            // action
+            Sword s = new Sword("こ");
+
+            Field name = s.getClass().getDeclaredField("name");
+            name.setAccessible(true);
+
+            assertEquals("名無し剣", name.get(s), "コンストラクタでの２文字以下でのフィールドnameの処理が不正です!");
+        } catch (Exception e) {
+            // NoSuchMethodException, IllegalAccessException iae はtestSetter()でチェックしているので、ここでは無視
             fail();
         }
     }
@@ -66,11 +89,14 @@ public class SwordTest {
             Sword s = new Sword("壊れた剣");
             Method getName = s.getClass().getMethod("getName");
 
-            assertEquals("壊れた剣", getName.invoke(s));
+            assertEquals("壊れた剣", getName.invoke(s),"getName()の戻り値が不正です!");
 
-        } catch (Exception e) {
-            // なにかしら例外がでたらfailにする
-            fail();
+        } catch (NoSuchMethodException nsme) {
+            fail("SwordクラスにgetName()が定義されていない、もしくはpublic宣言されていません! ");
+        } catch (IllegalAccessException iae) {
+            fail("SwordクラスのgetName()がpublic宣言されていません! ");
+        } catch (java.lang.reflect.InvocationTargetException ite) {
+            fail("getName()内で例外が発生しました"); // 教員対応
         }
     }
 }
