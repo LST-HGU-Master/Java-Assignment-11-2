797b7ab555f895d6071a6cf3dd72cbb9c386f69d
YoshiHirat <50605381+YoshiHirat@users.noreply.github.com>
2022-06-10 16:55:32 +0900

    Update ProgB2Test.java

--- src/test/java/ProgB2Test.java
+++ src/test/java/ProgB2Test.java
@@ -1,11 +1,12 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.Test;
 import java.io.*;
 
 public class ProgB2Test {
 
     @Test
-    public void testHelloWorld()
+    public void testMain()
     {
         PrintStream originalOut = System.out;
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
@@ -18,11 +19,16 @@ public class ProgB2Test {
         // in.inputln("2"); // 標準入力をテストする場合
         // Hello.main(new String[]{"1", "2", "3"}); // 実行時引数をテストする場合
         ProgB2.main(null);
-
-        // assertion
-        assertEquals("Hello world!\n", bos.toString());
-
         // undo the binding in System
         System.setOut(originalOut);
+        // assertion
+        String[] prints = bos.toString().split("\r\n|\n", -1);
+        try {
+            assertEquals("勇者太郎 (HP:100) が誕生した！",prints[0], "main()メソッドのprint文の１行目が実行例と異なっています!");
+            assertEquals("勇者はこんぼうを装備した！", prints[1],"main()メソッドのprint文の２行目が実行例と異なっています!");
+        } catch (ArrayIndexOutOfBoundsException er) {
+            fail("prin文の改行が実行例よりも不足しています!");
+        }
+        assertEquals(3, prints.length, "改行数が３つ以上あります!");
     }
 }
